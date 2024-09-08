83e98f7a92c60b07d97135d51e0fd8a775b51bdb
samp20096 <samp20096@gmail.com>
2024-09-08 20:02:11 +0300

    Add files via upload

---
+++ bboolean_puzzle.py
@@ -0,0 +1,12 @@
+# Start
+
+age: int = int(input("What's your age? "))
+
+age_aproved: bool = age > 18
+
+if age_aproved:
+    print("You're abouve 18")
+else:
+    print("You're not above 18 yet")
+
+# end
